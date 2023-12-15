d8d590521f6e41759b779b765a31a1065818e7da
s29862-pj <146528063+s29862-pj@users.noreply.github.com>
2023-12-15 20:45:34 +0100

    Add files via upload
    Wysłane zadania ze zjazdu 5.

---
+++ PPRG-zadanie5.1/PPRG-zadanie5.1/PPRG-zadanie5.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    
+    int n;
+    cout << "Wprowadź ilość elementów wektora: ";
+    cin >> n;
+
+    vector<int> vec(n);
+    cout << "Wprowadź elementy wektora, oddziel je spacją:" << endl;
+        for (int i = 0; i < n; ++i) {
+            cin >> vec[i];
+        }
+
+    int maxElement = vec[0];
+    int maxIndex = 0;
+
+        for (int i = 1; i < n; ++i) {
+            if (vec[i] > maxElement) {
+                maxElement = vec[i];
+                maxIndex = i;
+            }
+        }
+     
+     cout << "Największy element: " << maxElement << endl;
+     cout << "Numer indexu największego wektora: " << maxIndex << endl;
+
+        return 0;
+}
+
---
+++ PPRG-zadanie5.2/PPRG-zadanie5.2/PPRG-zadanie5.2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout << "Wprowadź liczbę elementów tablicy: ";
+    cin >> size;
+
+    if (size % 2 != 0) {
+        cerr << "Liczba musi być podzielna przez 2!" << endl;
+        return 1;
+    }
+
+    vector<int> array(size);
+    cout << "Wpisz elementy tablic oddzielając je spacją: " << endl;
+    for (int i = 0; i < size; ++i) {
+        cin >> array[i];
+    }
+
+    cout << "Dane wejściowe: " << endl;
+    for (int i = 0; i < size; i += 2) {
+        cout << array[i] << "\t" << array[i + 1] << endl;
+    }
+
+    for (int i = 0; i < size; i += 2) {
+        swap(array[i], array[i + 1]);
+    }
+
+    cout << "Dane po zmianie: " << endl;
+    for (int i = 0; i < size; i += 2) {
+        cout << array[i] << "\t" << array[i + 1] << endl;
+    }
+
+    return 0;
+}
